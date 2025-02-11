ea5e334dd425e5609fcf94466c83f6796e0bb959
Sean <mthulisisean@gmail.com>
2025-02-11 19:56:55 +0200

    Creating prelim database and adding 4 record

---
+++ InsertToTransaction.sql
@@ -0,0 +1,10 @@
+SELECT TOP (1000) [Transaction_No]
+      ,[Transaction_Description]
+      ,[Transaction_Amount]
+      ,[Transaction_Date]
+  FROM [Buse].[dbo].[Transanctions]
+
+  --Insert into [Buse].[dbo].[Transanctions] values 
+  --('Second Transaction','18.20','2024/02/11 09:30'),
+  --('Third Transaction','20.00','2024/02/11 11:30'),
+  --('Forth Transaction','30.00','2024/02/11 14:00')
